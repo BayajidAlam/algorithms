f747f29bf4a8b886ae76546ef4840ebfab8be1f2
BayajidAlam <bayajidalam2001@gmail.com>
2024-02-05 01:17:04 +0600

    cycle-detection

---
+++ cycle-detection/cycle-detect-using-dfs.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+bool vis[N];
+vector<int> adj[N];
+int parent_Array[N];
+bool ans;
+
+void dfs(int parent)
+{
+  vis[parent] = true;
+  for (int child : adj[parent])
+  {
+    if (vis[child] == true && child != parent_Array[parent])
+    {
+      ans = true;
+      cout << parent << " " << child << endl;
+    }
+    if (vis[child] == false)
+    {
+      parent_Array[child] = parent;
+      dfs(child);
+    }
+  }
+}
+
+int main()
+{
+
+  int n, e;
+  cin >> n >> e;
+  while (e--)
+  {
+    int a, b;
+    cin >> a >> b;
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+  }
+
+  memset(vis, false, sizeof(vis));
+  memset(parent_Array, -1, sizeof(parent_Array));
+
+  ans = false;
+  for (int i = 0; i < n; i++)
+  {
+    if (!vis[i])
+    {
+      dfs(i);
+    }
+  }
+  if (ans)
+    cout << "Cyclic" << endl;
+  else
+    cout << "Not cyclic" << endl;
+  return 0;
+}
