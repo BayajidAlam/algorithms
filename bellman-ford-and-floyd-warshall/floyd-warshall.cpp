446506fe2768cb27531dc4ce45498c9104a92bca
BayajidAlam <bayajidalam2001@gmail.com>
2024-02-05 22:48:03 +0600

    learning floyd warshall

---
+++ bellman-ford-and-floyd-warshall/floyd-warshall.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+  int n, e;
+  cin >> n >> e;
+  int adj[n][n];
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      adj[i][j] = INT_MAX;
+    }
+  }
+  while (e--)
+  {
+    int a, b, c;
+    cin >> a >> b >> c;
+    adj[a][b] = c;
+  }
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      cout << adj[i][j] << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
