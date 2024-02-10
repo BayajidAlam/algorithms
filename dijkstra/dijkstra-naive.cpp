e5a4513aabaf890754495de01ac5dd29a850b1a7
BayajidAlam <bayajidalam2001@gmail.com>
2024-02-11 02:01:38 +0600

    updating dijkstra algorithm

--- dijkstra/dijkstra-naive.cpp
+++ dijkstra/dijkstra-naive.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 const int N = 1e5 + 5;
 vector<pair<int, int>> v[N];
+// track the distance
 int dis[N];
 
+// the func
 void dijkstra(int src)
 {
+  // push the pair to queue and add dis
   queue<pair<int, int>> q;
   q.push({src, 0});
   dis[src] = 0;
@@ -17,12 +20,20 @@ void dijkstra(int src)
     q.pop();
     int node = parent.first;
     int cost = parent.second;
+    // for (int i = 0; i < v[node].size(); i++)
+    // {
+
+    // }
+
+    // Alternative way
     for (pair<int, int> child : v[node])
     {
       int childNode = child.first;
       int childCost = child.second;
       if (cost + childCost < dis[childNode])
       {
+        // path relax
+        // update the new minimum dis
         dis[childNode] = cost + childCost;
         q.push({childNode, dis[childNode]});
       }
@@ -39,19 +50,24 @@ int main()
   {
     int a, b, c;
     cin >> a >> b >> c;
+    // push value with cost
     v[a].push_back({b, c});
     v[b].push_back({a, c});
   }
 
+  // set default value
   for (int i = 0; i < n; i++)
   {
     dis[i] = INT_MAX;
   }
+
+  // dijkstra func
   dijkstra(0);
 
+  // check
   for (int i = 0; i < n; i++)
   {
-    cout << i << " " << dis[i] << endl;
+    cout << i << "-> " << dis[i] << endl;
   }
   return 0;
 }
